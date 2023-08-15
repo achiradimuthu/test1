2862ff61aab2bb162bd66d0c4cc54e6f0ccf59b9
Achira_Dimuthu <bvachiradimuthu@gmail.com>
2023-08-16 00:15:50 +0530

    Add contact.php file

---
+++ contact.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test 1 | contact</title>
+    </head>
+    <body>
+        <h1>This is contact page</h1>
+        <a href="index.php">Click to go about page</a>
+    </body>
+</html>
